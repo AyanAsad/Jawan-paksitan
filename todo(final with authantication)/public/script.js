a06d31f0c5e89d829e65f70c3d9daeb04fd11448
AyanAsad <ayan47816@gmail.com>
2024-11-13 08:36:38 +0500

    name changes

---
+++ todo(final with authantication)/public/script.js
@@ -0,0 +1,19 @@
+/*<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBJpVJsARf2YaLB58t0THDKx85Fwdnuftc",
+    authDomain: "form-5f5db.firebaseapp.com",
+    projectId: "form-5f5db",
+    storageBucket: "form-5f5db.firebasestorage.app",
+    messagingSenderId: "982359202777",
+    appId: "1:982359202777:web:79ae811d6dd95a7baa17c8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
